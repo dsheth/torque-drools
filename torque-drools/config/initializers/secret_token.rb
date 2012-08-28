76973614ce10d6f08b6940baaf1204dbae6bb3b8
David Sheth <david@dsheth.com>
2012-08-27 23:41:31 -0500

    Initial commit of newly created project

---
+++ torque-drools/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TorqueDrools::Application.config.secret_token = '7a7b40877d62cccbd207faea1a6c506563d314b64758dddc19763a5c007c6b345d33e10a2592ee4c0bf5b2693247ef792e86ea8bfc87f9144648ba3ea193fdcb'
